3189fa4d4647165e532e35226cca5ec94fbffd97
Daniel Machin <DanielTAYG@gmail.com>
2024-01-12 16:13:08 -0500

    skills section

--- src/app/knowledge/knowledge.component.ts
+++ src/app/knowledge/knowledge.component.ts
@@ -32,6 +32,30 @@ export class KnowledgeComponent {
       "image": "../../assets/angular.png",
       "name": "Angular"
     },
+    {
+      "image": "../../assets/HTML.png",
+      "name": "HTML"
+    },
+    {
+      "image": "../../assets/css.png",
+      "name": "CSS"
+    },
+    {
+      "image": "../../assets/JavaScript-logo.png",
+      "name": "JavaScript"
+    },
+    {
+      "image": "../../assets/typescript.png",
+      "name": "TypeScript"
+    },
+    {
+      "image": "../../assets/python.png",
+      "name": "Python"
+    },
+    {
+      "image": "../../assets/angular.png",
+      "name": "Angular"
+    }
     // {
     //   "image": "../../assets/react.png",
     //   "name": "React"
@@ -39,6 +63,22 @@ export class KnowledgeComponent {
   ]
 
   tools: any[] = [
+    {
+      "image": "../../assets/AWS.png",
+      "name": "AWS"
+    },
+    {
+      "image": "../../assets/TailwindCSS.png",
+      "name": "TailwindCSS"
+    },
+    {
+      "image": "../../assets/VSCODE.svg",
+      "name": "VS Code"
+    },
+    {
+      "image": "../../assets/Postman.svg",
+      "name": "Postman"
+    },
     {
       "image": "../../assets/AWS.png",
       "name": "AWS"
@@ -77,7 +117,28 @@ export class KnowledgeComponent {
       {
         "image": "../../assets/MongoDB.svg",
         "name": "MongoDB"
+      },
+      {
+        "image": "../../assets/Django.png",
+        "name": "Django"
+      },
+      {
+        "image": "../../assets/PostgreSQL.png",
+        "name": "PostgreSQL"
+      },
+      {
+        "image": "../../assets/NodeJS.png",
+        "name": "NodeJS"
+      },
+      {
+        "image": "../../assets/NPM.png",
+        "name": "NPM"
+      },
+      {
+        "image": "../../assets/MongoDB.svg",
+        "name": "MongoDB"
       }
+
     
   ]
   
