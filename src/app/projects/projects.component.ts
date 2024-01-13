cf1d5105b312723cfd3e1101a63cb7f1bd77c797
Daniel Machin <DanielTAYG@gmail.com>
2024-01-13 18:24:47 -0500

    submit

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -13,32 +13,32 @@ export class ProjectsComponent {
       "name": "FitFolio",
       "description": "FitFolio is a FullStack application used for searching workouts in an extensive library",
       "icon": `../../assets/fitfolio.png`,
-      "live-site": "placeholder",
-      "github": "placeholder"
+      "livesite": "https://fit-folio.netlify.app/",
+      "github": "https://github.com/Dmachin7/Fitfolio-Frontend"
     },
     {
       "image": "../../assets/adventurelist.jpg",
       "name": "AdventureList",
       "description": "AdventureList is a FullStack application used for keeping track of Adventures",
       "icon": `../../assets/hikerman.png`,
-      "live-site": "placeholder",
-      "github": "placeholder"
+      "livesite": "https://adventurelist-01b3f64537ec.herokuapp.com/",
+      "github": "https://github.com/User5678953/adventure-list-frontend"
     },
     {
       "image": "../../assets/pavers.jpg",
       "name": "We Do Pavers",
       "description": "We Do Pavers is a Frontend application used for a local business to market their services",
       "icon": `../../assets/paver-icon.png`,
-      "live-site": "placeholder",
-      "github": "placeholder"
+      "livesite": "https://wedopavers.today/",
+      "github": "https://github.com/Dmachin7/We_Do_Pavers"
     },
     {
       "image": "../../assets/foodle.jpg",
       "name": "Foodle",
       "description": "Foodle is a application used for searching a database of recipes",
       "icon": `../../assets/restaurant.png`,
-      "live-site": "placeholder",
-      "github": "placeholder"
+      "livesite": "https://fooodle.netlify.app/",
+      "github": "https://github.com/Dmachin7/Foodle"
     }
   ]
 
